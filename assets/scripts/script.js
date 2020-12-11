3ee3ae4fb34f8d64f7f2895008b79c464ce8a9c8
Kdoggg666 <kenanwright@gmail.com>
2020-12-11 18:59:19 +0000

    Add clock

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,3 +1,5 @@
+// ---------------- Drumkit ---------------
+
 window.addEventListener('keydown', function(e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
@@ -15,3 +17,29 @@ function removeTransition(e){
 
 const keys = document.querySelectorAll('.key');
 keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+
+// ------------- Clock -----------
+
+const secondHand = document.querySelector(".second-hand")
+const minuteHand = document.querySelector(".min-hand")
+const hourHand = document.querySelector(".hour-hand")
+
+function setDate() {
+    const now = new Date();
+
+    const seconds = now.getSeconds();
+    const secondsDegrees = ((seconds / 60) * 360) + 90;
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+    const minutes = now.getMinutes();
+    const minutesDegrees = ((minutes / 60) * 360) + 90;
+    minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
+
+    const hours = now.getHours();
+    const hoursDegrees = ((hours / 12) * 360) + 90;
+    hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+
+}
+
+setInterval(setDate, 1000);
